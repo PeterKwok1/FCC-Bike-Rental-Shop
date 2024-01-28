ea26d06fcc27cfaa83d271de3d17ada8ac8d46dc
Peter Kwok <peter.kwok94@gmail.com>
2024-01-28 16:53:08 -0500

    Save progress: 1236.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -57,6 +57,11 @@ RENT_MENU() {
       then
         # send to main menu
         MAIN_MENU "That bike is not available."
+      else 
+        # get customer info
+        # if customer doesn't exist
+        # get new customer name
+        # insert new customer
       fi
     fi
   fi
