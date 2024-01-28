6ba0b6cc2ea50fa6165e3f5dd9f5333b60a495a6
Peter Kwok <peter.kwok94@gmail.com>
2024-01-28 17:00:42 -0500

    Save progress: 1290.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -71,6 +71,7 @@ RENT_MENU() {
           echo -e "\nWhat's your name?"
           read CUSTOMER_NAME
           # insert new customer
+          INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(phone, name) VALUES('$PHONE_NUMBER', '$CUSTOMER_NAME')")
         fi
       fi
     fi
