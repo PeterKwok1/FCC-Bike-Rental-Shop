9fe6482ddcba3a50cfbb6da4be5fdca81db8f055
Peter Kwok <peter.kwok94@gmail.com>
2024-01-28 01:14:30 -0500

    Save progress: 1105.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -31,6 +31,7 @@ RENT_MENU () {
     MAIN_MENU "Sorry, we don't have any bikes available right now."
   else
     # display available bikes
+    echo -e "\nHere are the bikes we have available:"
     # ask for bike to rent
     # if input is not a number 
     # send to main menu
