6757d541e2b2f61a4c2ab06459e4e6587ef929ac
Peter Kwok <peter.kwok94@gmail.com>
2024-01-28 16:56:09 -0500

    Save progress: 1260.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -61,6 +61,7 @@ RENT_MENU() {
         # get customer info
         echo -e "\nWhat's your phone number?"
         read PHONE_NUMBER
+        CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$PHONE_NUMBER'")
         # if customer doesn't exist
         # get new customer name
         # insert new customer
