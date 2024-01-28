1173aa2b2b504b89749dc57790f49fa1f6eebdc2
Peter Kwok <peter.kwok94@gmail.com>
2024-01-28 17:20:13 -0500

    Save progress: 1401.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -80,6 +80,7 @@ RENT_MENU() {
         # set bike availability to false
         SET_TO_FALSE_RESULT=$($PSQL "UPDATE bikes SET available=false WHERE bike_id=$BIKE_ID_TO_RENT")
         # get bike info
+        BIKE_INFO=$($PSQL "SELECT size, type FROM bikes WHERE bike_id=$BIKE_ID_TO_RENT")
         # send to main menu
       fi
     fi
