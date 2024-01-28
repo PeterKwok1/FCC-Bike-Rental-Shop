26ce5b691e2f1aaf67a7a748206111064c5aaedb
Peter Kwok <peter.kwok94@gmail.com>
2024-01-28 01:06:27 -0500

    Save progress: 1000.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -25,7 +25,10 @@ RENT_MENU () {
   AVAILABLE_BIKES=$($PSQL "SELECT bike_id, type, size FROM bikes WHERE available=true ORDER BY bike_id")
   echo "$AVAILABLE_BIKES"
   # if no bikes available
+  if [[ -z $AVAILABLE_BIKES ]]
+  then
   # send to main menu
+  fi
 }
 
 RETURN_MENU () {
