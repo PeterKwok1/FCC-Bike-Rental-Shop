a6acfe59ab19622d655058449bcd5995e0610474
Peter Kwok <peter.kwok94@gmail.com>
2024-01-29 19:48:50 -0500

    Save progress: 1602.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -124,7 +124,10 @@ RETURN_MENU() {
       echo -e "\nWhich one would you like to return?"
       read BIKE_ID_TO_RETURN
       # if not a number
-      # send to main menu
+      if [[ ! $BIKE_ID_TO_RETURN =~ ^[0-9]+$ ]]
+      then
+        # send to main menu
+      fi
     fi
   fi
 }
