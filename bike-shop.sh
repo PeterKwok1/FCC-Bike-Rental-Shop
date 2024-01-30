44e57baacf1459846ae3b4637f4a1c50b6a5b376
Peter Kwok <peter.kwok94@gmail.com>
2024-01-30 15:35:26 -0500

    Save progress: 1660.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -137,7 +137,7 @@ RETURN_MENU() {
           # send to main menu
           MAIN_MENU "You do not have that bike rented."
         else
-          echo Rental ID $RENTAL_ID found
+
         fi
       fi
     fi
