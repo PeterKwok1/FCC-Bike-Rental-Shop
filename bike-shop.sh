e25e0ab0a30c270e46b880bcbc8104664eb86b9c
Peter Kwok <peter.kwok94@gmail.com>
2024-01-29 20:03:20 -0500

    Save progress: 1630.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -131,6 +131,7 @@ RETURN_MENU() {
       else
         # check if input is rented
         
+        RENTAL_ID=$($PSQL "SELECT rental_id FROM rentals INNER JOIN customers USING(customer_id) WHERE phone = '$PHONE_NUMBER' AND bike_id = $BIKE_ID_TO_RETURN AND date_returned IS NULL")
         # if input not rented
         # send to main menu
       fi
