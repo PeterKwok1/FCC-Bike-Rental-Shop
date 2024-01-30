649a6795414ff5ac24c361015340af3f3f4d015f
Peter Kwok <peter.kwok94@gmail.com>
2024-01-30 15:35:49 -0500

    Save progress: 1680.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -137,7 +137,9 @@ RETURN_MENU() {
           # send to main menu
           MAIN_MENU "You do not have that bike rented."
         else
-
+          # update date_returned
+          # set bike availability to true
+          # send to main menu
         fi
       fi
     fi
